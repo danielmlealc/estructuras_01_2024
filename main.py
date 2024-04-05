94134219a1745daaa8e7564f4d4d6b5d094cf0c9
danielmlealc <142677210+danielmlealc@users.noreply.github.com>
2024-04-04 19:46:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def read_root():
 def read_item(data: str | None = Query(None, min_length=3)):
     return {"tutelas": data}
 
-@app.get("/api/v1/search")
+@app.get("/api/v1/search/tipo_tutela")
 def read_item(tipo_tutela: str | None = Query(None, min_length=3)):
     result = []
     for tutela in tutelas:
