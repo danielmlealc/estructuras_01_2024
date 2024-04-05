2eb550b958ccc8b2a0e5ec849fc4b952ee4dd52d
danielmlealc <142677210+danielmlealc@users.noreply.github.com>
2024-04-04 19:35:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,17 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/data{data}")
-def read_root():
+def read_root(data: string):
     return {"tutelas": data}
 
 @app.get("/api/v1/search?tipo_tutela=salud")
 def read_root():
-    def find_decreto(keyword):
-        result = []
-        for tutela in tutelas:
-            if re.search(keyword, tutela["titulo"] + " " + tutela["resumen"], re.IGNORECASE):
-                result.append(tutela)
-        return result
+    result = []
+    for tutela in tutelas:
+        if re.search(tipo_tutela, tutela["titulo"] + " " + tutela["resumen"], re.IGNORECASE):
+            result.append(tutela)
 
-return find_decreto(tipo_tutela)
+return result
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
